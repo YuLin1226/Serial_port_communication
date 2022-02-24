5ff8d72ecbd481d876755f72bdf81be9aac25827
RyanChen_YLC <r08522622@ntu.edu.tw>
2022-02-24 10:19:13 +0800

    new modbus rtu cpp files

---
+++ modbus.cpp
@@ -0,0 +1,232 @@
+
+#include "modbus.h"
+
+#define WRITE_DELAY 10000
+
+namespace Motor
+{
+    // 建構元
+    SerialModbus::SerialModbus(const std::string _serial_port, const int _baud_rate) {
+        set_param_serial(_serial_port, _baud_rate);
+    }
+    // 解構元：關閉port
+    SerialModbus::~SerialModbus() {
+        close();
+    }
+
+    void SerialModbus::set_param_serial(const std::string _serial_port, const int _baud_rate) {
+        p_serial_port = _serial_port;
+        p_baud_rate = _baud_rate;
+    }
+
+    int SerialModbus::open() {
+        if(p_port != NULL) {
+            return -1;
+        }
+
+        if(p_service) {
+            p_port.reset();
+            p_service.reset();
+        }
+            
+        p_mutex = std::shared_ptr<boost::mutex>{new boost::mutex};
+        p_service = std::shared_ptr<io_service>{new io_service()};
+        p_port = std::shared_ptr<serial_port>{ new serial_port(*p_service) };
+
+        try 
+        {
+            p_port->open(p_serial_port);
+            p_port->set_option(serial_port_base::baud_rate(p_baud_rate));
+            // 數據位
+            p_port->set_option(serial_port_base::character_size(8));
+            // 奇偶校驗
+            p_port->set_option(serial_port_base::parity(serial_port_base::parity::none));
+            // 停止位
+            p_port->set_option(serial_port_base::stop_bits(serial_port_base::stop_bits::one));
+            return 0;
+        }
+        catch (std::exception &ex){
+            std::cout<<"open exception : "<<ex.what()<<std::endl;
+        }
+    }
+
+    int SerialModbus::close() {
+        if(p_port && p_port->is_open()) {
+             p_port->close();
+        }           
+    }
+
+
+
+
+
+
+
+
+    /*
+    ========================= 待刪除 =========================
+    */
+
+
+
+    // // 5引數：函數呼叫順序：{ single_register_read >> single_register_write >> write >> read } 即從port讀取資料
+    // // 引數分別為：起始位(0)、站號(1,2)、功能碼(3,4)、地址、資料
+    // // Q：地址（_address）和資料（_data）分別是什麼？
+    // // A：暫存器地址 & 讀到的資料數據
+    // std::vector<u_int16_t> SerialModbus::single_register_read(const char *_start_byte, const char *_id, const char *_function_code, const char *_address, const char *_data) {
+    //     // auto 運算子會讓系統自動判別資料型態
+    //     // p_read_data 是 SerialPort 讀到的東西
+    //     // 引數的部份是因為需要計算LRC，確認傳輸無誤。
+    //     auto p_read_data = single_register_write(_start_byte, _id, _function_code, _address, _data);
+    //     // .at(i) 讀取第i個元素，此函數會自動作邊界處理，相對安全。
+    //     // Q：p_read_data 第6、7格資料是？？
+    //     char data_size[2]={p_read_data.at(5), p_read_data.at(6)};
+    //     // 把 data_size 從 ascii 制轉成 16 進制
+    //     int p_data_size = ascii_2_data(data_size);
+    //     std::vector<u_int16_t> p_data_set;
+    //     // 這一個迴圈在把資料作解碼（ascii > hex）
+    //     // Q：但依照的規則是？
+    //     for(int i=0; i < p_data_size / 2; i+=4) 
+    //     {
+    //         auto data = ascii4_2_data(&p_read_data.at(i+7));
+    //         p_data_set.push_back(data);
+    //     }
+    //     // 回傳解碼完的 hex 資料。
+    //     return p_data_set;
+    // }
+    // // 2引數：呼叫5引數的同名函數。
+    // std::vector<u_int16_t> SerialModbus::single_register_read(const char *_address, const char *_data) {
+    //     return single_register_read(":", MODBUS_ID, "03", _address, _data);
+    // }
+
+    // std::vector<char> SerialModbus::read() {
+    //     try
+    //     {
+    //         const int max_char{ 64 };
+    //         char buff[max_char]{ 0 };
+    //         boost::mutex::scoped_lock sl(*p_mutex);
+    //         size_t total_size = 0;
+    //         std::vector<char> p_char;
+    //         p_char.clear();
+    //         while(p_port->is_open())
+    //         {
+    //             // 從port讀取資料
+    //             auto read_size = p_port->read_some(boost::asio::buffer(buff, max_char));
+    //             if(read_size > 0)
+    //             {
+    //                 // 複製buff一部分資料到p_char_tmp
+    //                 std::vector<char> p_char_tmp(buff, buff+read_size);
+    //                 // 把p_char_tmp資料插入到p_char後端（應該是後端吧？！）
+    //                 p_char.insert(p_char.end(), p_char_tmp.begin(), p_char_tmp.end());
+    //                 // 如果p_char_tmp的最末位為10，回傳p_char
+    //                 if(p_char_tmp.at(p_char_tmp.size()-1)==10) {
+    //                     return p_char;
+    //                 }
+    //             }
+    //             else 
+    //             {
+    //                 usleep(1000);
+    //                 continue;
+    //             }                
+    //         }  
+    //         throw "Serial Port Is Not Open";
+    //     }
+    //     catch (const std::exception& ex)
+    //     {
+    //         std::cout << "Read exception " << ex.what() << std::endl;
+    //     }
+    // }
+    // // 1引數Write：這應該是把資料寫進去的部份？？
+    // std::vector<char> SerialModbus::write(std::vector<char> _data) {
+    //     // 這個函數包含了寫入和讀取SerialPort，應該是在確認送出的資料無誤？？
+	//     if (p_port->is_open()) { 
+    //         for(auto i=0; i < _data.size(); i++) 
+    //         {
+    //             // toupper是把字元轉換為大寫的函數：a -> A
+    //             _data[i] = (char)toupper(_data[i]);
+    //         }
+    //         {
+    //             boost::mutex::scoped_lock lock(*p_mutex);
+    //             auto size = p_port->write_some(buffer(_data));
+    //             //printf("write_size: %d\n", (int)size);
+    //             if(_data.size() != size) {
+    //                 throw "Write Size Error!!!";
+    //             }                    
+    //         }      
+    //         usleep(100);
+    //         // 從port讀取資料
+    //         auto p_read_data = read();
+    //         // 資料的第四、第五位是功能碼
+    //         if(p_read_data.at(3) == '0' && p_read_data.at(4) == '6') {
+    //             for(auto i = p_read_data.size()-2; i < p_read_data.size(); i++) 
+    //             {
+    //                 // 資料異常確認：檢驗碼（末兩碼）確認（收到的資料和寫出的資料應該要相同）
+    //                 if(p_read_data.at(i) != _data.at(i)) {
+    //                     throw "Data Write Error";                        
+    //                 }
+    //             }
+    //         }         
+    //         return p_read_data;
+    //     }
+    //     throw "Port not open";
+    // }
+
+    // // 5引數Write
+    // std::vector<char> SerialModbus::write(const char *_start_byte, const char *_id, const char *_function_code, const char *_data, const int _data_segment) {
+    //     std::vector<char> p_data(0);
+
+    //     // 堆疊資料p_data，應是follow Modbus協議的格式
+    //     // 起始碼
+    //     p_data.push_back(_start_byte[0]);
+    //     // 站號
+    //     p_data.push_back(_id[0]);
+    //     p_data.push_back(_id[1]);
+    //     // 功能碼
+    //     p_data.push_back(_function_code[0]);
+    //     p_data.push_back(_function_code[1]);
+    //     // 資料內容
+    //     for(auto i = 0; i < _data_segment; i++) {
+    //         p_data.push_back(_data[i]);
+    //     }
+    //     // 檢驗碼
+    //     char p_lrc_hi{0};
+    //     char p_lrc_lo{0};
+    //     lrc(_id, _function_code, _data, _data_segment, &p_lrc_hi, &p_lrc_lo);
+    //     p_data.push_back(p_lrc_hi);
+    //     p_data.push_back(p_lrc_lo);
+    //     // \r：（回車）回到行首、\n：跳至下一行；加在一起就是跑到下一行行首，也就是ENTER的基本功能。
+    //     // Q：不過為什麼需要這個？？
+    //     // A：作為結束符，CR LF。
+    //     p_data.push_back('\r');
+    //     p_data.push_back('\n');
+    //     // 呼叫1引數Write：return SerialPort讀到的東西
+    //     return write(p_data);
+    // }
+    // // 5引數
+    // std::vector<char> SerialModbus::single_register_write(const char *_start_byte, const char *_id, const char *_function_code, const char *_address, const char *_data) {
+    //     const int p_data_segment{8};
+    //     const int p_address_bytes{4};
+    //     char p_data[8];
+    //     // 把_address 內容複製到 p_data 的前4格。
+    //     for(auto i = 0; i < p_address_bytes; i++) {
+    //         p_data[i] = _address[i];
+    //     }
+    //     // 把_data 內容複製到 p_data 的後4格。
+    //     for(auto i = 0; i < p_data_segment - p_address_bytes; i++) {
+    //         p_data[i+4] = _data[i];
+    //     }
+    //     // return SerialPort 讀到的東西
+    //     return write(_start_byte, _id, _function_code, p_data, p_data_segment);
+    // }
+    // // 2引數：呼叫5引數的同名函數。
+    // std::vector<char> SerialModbus::single_register_write(const char *_address, const char *_data) {
+    //     return single_register_write(":", MODBUS_ID, "06", _address, _data);
+    // }
+
+    /*
+    ========================= 待刪除 =========================
+    */
+
+
+
+}
---
+++ modbus.h
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+#include <numeric>
+#include <sstream>
+#include <iomanip>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+
+
+#define _DEBUG
+#ifdef _DEBUG
+#define __DEBUG__ true
+#else
+#define __DEBUG__ false
+#endif
+
+
+#ifndef __TRUMMAN_H__
+#define __TRUMMAN_H__
+
+using namespace boost::asio;
+
+namespace Motor
+{
+    class SerialModbus
+    {
+        protected:
+            /*
+            shared_ptr 是C++11提供的一種智能指針類，可以在任何地方都不使用時自動刪除相關指針，從而幫助徹底消除內存泄漏和懸空指針的問題。
+            */
+            std::shared_ptr<io_service> p_service;
+            std::shared_ptr<serial_port> p_port;
+            std::shared_ptr<boost::mutex> p_mutex;
+            std::shared_ptr<boost::mutex> p_func_mutex;
+            std::string p_serial_port;
+            int p_baud_rate;
+
+        public:
+            SerialModbus(const std::string _serial_port, const int _baud_rate);
+            virtual ~SerialModbus();
+
+        public:
+            void set_param_serial(const std::string _serial_port, const int _baud_rate);
+            int open();
+            int close();
+
+
+            /*
+            ========================= 待刪除 =========================
+            */
+
+            // std::vector<char> read();
+            // std::vector<u_int16_t> single_register_read(const char *_start_byte, const char *_id, const char *_function_code, const char *_address, const char *_data);
+            // std::vector<u_int16_t> single_register_read(const char *_address, const char *_data);
+
+            // std::vector<char> write(std::vector<char> _data);
+            // std::vector<char> write(const char *_start_byte, const char *_id, const char *_function_code, const char *_data, const int _data_segment);
+            // std::vector<char> single_register_write(const char *_start_byte, const char *_id, const char *_function_code, const char *_address, const char *_data);
+            // std::vector<char> single_register_write(const char *_address, const char *_data);
+
+            /*
+            ========================= 待刪除 =========================
+            */
+
+        protected:
+
+
+            inline uint16_t calculate_crc(std::vector<uint16_t> _data, int len){ 
+
+                uint16_t crc = 0xFFFF;  
+                for(auto idx=0; idx<len; idx++){
+                    crc ^= (uint16_t)_data[idx];
+                    for(auto i=8; i!=0; i--){
+
+                        if((crc & 0x0001) != 0){
+                            crc >>= 1;
+                            crc ^= 0xA001;
+                        }
+                        else{
+                            crc >>= 1;
+                        }
+                    }
+                }
+                return crc;
+            }
+
+
+
+            /*
+            ================================== 待刪除 =====================================
+            */
+
+            // inline int ascii_2_data(const char *_ascii) {
+            //     // 把_ascii的前兩個字元放到p_data裡
+            //     const char p_data[] = {_ascii[0], _ascii[1], '\0'};
+            //     // 搜尋p_data前面字串的數字部份，並以16進宣告該數字。
+            //     int number = (int)strtol(p_data, NULL, 16);
+            // }
+            // inline int ascii4_2_data(const char *_ascii) {
+            //     // 概念同上
+            //     const char p_data[] = {_ascii[0], _ascii[1], _ascii[2], _ascii[3], '\0'};
+            //     int number = (int)strtol(p_data, NULL, 16);
+            // }
+            // inline void data_2_ascii(int _data, char *_ascii) {
+            //     /*
+            //     這個函數就是把_data以16進存到_ascii裡面。
+            //     */
+            //     const int MAX_BYTES{4};
+            //     // stringstream 可以想成是轉換字串的容器，也可以想成是轉換器。
+            //     std::stringstream ss{};
+            //     // setw 是設定字串長度，不足該長度，前面會補空格；setfill把空格改成用0補。
+            //     // 所以字串前面是0，後面是16進制的_data，例如：000000001f4 這種概念。
+            //     ss << std::setw(MAX_BYTES) << std::setfill('0') << std::hex << (int)_data;
+            //     // 把ss的字串複製到str裡。
+            //     std::string str = ss.str();
+            //     // 再把str的字串一個一個放到ascii裡。
+            //     for(auto i=0; i<MAX_BYTES; i++)
+            //         _ascii[i] = str.c_str()[i];
+            // }
+            // // 四引數
+            // inline unsigned char lrc(const char *_id, const char *_function_code, const char *_data, const int _data_segment) {
+            //     std::vector<int> p_data;
+            //     // p_data 會儲存從ascii轉譯為hex的 _id, _function_code, _data, _data_segment 資料，然後就可以拿來算 LRC 。
+            //     p_data.push_back(ascii_2_data(_id));
+            //     p_data.push_back(ascii_2_data(_function_code));                
+            //     for(auto i = 0; i < _data_segment; i+=2) {
+            //         p_data.push_back(ascii_2_data(_data+i));
+            //     }
+            //     // Q：這一部份要搞懂的是：p_lrc是用來幹麻的？ 
+            //     // A：LRC 用來當檢查碼，因為沒有轉換器可以自動生成，所以需要自己計算。
+            //     // 把accumulate函數把p_data作加總，加總後和0x00FF作按位與，再用256減去結果，可得LRC檢驗碼。
+            //     unsigned char p_lrc = 256 -(std::accumulate(p_data.begin(), p_data.end(), 0) & 0x00FF);
+            //     return p_lrc;
+            // }
+            // // 三引數
+            // inline void lrc(const unsigned char _lrc, char *_lrc_hi, char *_lrc_lo) {
+            //     const int MAX_BYTES{2};
+            //     // stringstream 可以想成是轉換字串的容器，也可以想成是轉換器。
+            //     std::stringstream ss{};
+            //     // setw 是設定字串長度，不足該長度，前面會補空格；setfill把空格改成用0補。
+            //     // 所以字串前面是0，後面是16進制的_lrc，例如：000000001f4 這種概念。
+            //     ss << std::setw(MAX_BYTES) << std::setfill('0') << std::hex << (int)_lrc;
+            //     // 把ss的字串複製到str裡。
+            //     std::string str = ss.str();
+            //     // 分別把str的前兩個字元取出、放入指標，作為通訊協定中的LRC高位與低位。
+            //     *_lrc_hi = str.c_str()[0];
+            //     *_lrc_lo = str.c_str()[1];
+            // }
+            // // 六引數
+            // inline unsigned char lrc(const char *_id, const char *_function_code, const char *_data, const int _data_segment, char *_lrc_hi, char *_lrc_lo) {
+            //     // 呼叫四引數lrc
+            //     auto p_lrc = lrc(_id, _function_code, _data, _data_segment);
+            //     // 呼叫三引數lrc
+            //     lrc(p_lrc, _lrc_hi, _lrc_lo);
+            //     // Q：這邊是不是少寫一個return阿？
+            //     /* A： 
+            //         > 應該沒差，三引數lrc要的是把p_lrc的資料寫進去_lrc_hi, _lrc_lo兩個裡面。
+            //         > 所以它把這個函式當成過程，然後引數是指標，所以還是更動得到資料。
+            //         > 我自己覺得，如果應要寫，大概是加個return;而已。
+            //     */
+            // }
+
+            /*
+            ================================== 待刪除 =====================================
+            */
+
+
+    };
+#endif
