b8f0fc30f16559b1e33bb5f5fb05ee0e086705d0
RyanChen_YLC <r08522622@ntu.edu.tw>
2022-04-13 08:26:26 +0800

    debug - encoder data received.

--- motor_driver.cpp
+++ motor_driver.cpp
@@ -668,9 +668,8 @@ namespace Motor{
         encoder_step_._data_byte[1]     = response.at(4);
         encoder_step_._data_byte[0]     = response.at(5);
         std:: cout << "=========\n";
-        std:: cout <<encoder_index_._data << " " << encoder_step_._data <<"\n";
-        encoder_data_ = (encoder_index_._data + encoder_step_._data/10000)*M_PI/180;
-
+        std:: cout << std::dec << encoder_index_._data << " " << encoder_step_._data <<"\n";
+        encoder_data_ = (encoder_index_._data + encoder_step_._data/10000.0)*2.0*M_PI;
         return encoder_data_;
     }
 
