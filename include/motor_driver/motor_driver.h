eb8cbea7f8327977217947d0910cca491660e480
Ryan Chen <b04502132@ntu.edu.tw>
2022-07-08 10:32:59 +0800

    update motor_driver example code

--- include/motor_driver/motor_driver.h
+++ include/motor_driver/motor_driver.h
@@ -1,148 +1,79 @@
-#include "modbus.h"
+#include "../serial_port_communication.h"
 
-#ifndef MOTOR_DRIVER_H
-#define MOTOR_DRIVER_H
+#ifndef _MOTOR_DRIVER_H
+#define _MOTOR_DRIVER_H
 
 
-namespace Motor
+namespace AMR
 {
-    class MotorDriver : public SerialModbus
+    class MotorDriver : public Communication::SerialPort
     {
     private:
-        std::shared_ptr<boost::mutex> p_func_mutex;
+        std::shared_ptr<boost::mutex> func_mutex_;
 
-        const uint8_t broadcast_            = 0x00;
-        const uint8_t motor_id_             = 0x01; // this shouldn't be const, cuz it's different for different motors.
-        const uint8_t fc_master_send_cmd_   = 0x65;
-        const uint8_t cmd_ISTOP_echo_       = 0x00;
-        const uint8_t cmd_ISTOP_no_echo_    = 0x64;
-        const uint8_t cmd_JG_echo_          = 0x0A;
-        const uint8_t cmd_JG_no_echo_       = 0x6E;
-        const uint8_t cmd_FREE_echo_        = 0x05;
-        const uint8_t cmd_FREE_no_echo_     = 0x69;
-        const uint8_t cmd_SVON_echo_        = 0x06;
-        const uint8_t cmd_SVON_no_echo_     = 0x6A;
-        const uint8_t cmd_SVOFF_echo_       = 0x07;
-        const uint8_t cmd_SVOFF_no_echo_    = 0x6B;
-        const uint8_t cmd_IMR_echo_         = 0x0B;
-        const uint8_t cmd_IMR_no_echo_      = 0x6F;
-        const uint8_t cmd_CS_echo_          = 0x0E;
-        const uint8_t cmd_CS_no_echo_       = 0x72;
-        const uint8_t cmd_CMR_echo_         = 0x0F;
-        const uint8_t cmd_CMR_no_echo_      = 0x73;
-        const uint8_t cmd_CMA_echo_         = 0x10;
-        const uint8_t cmd_CMA_no_echo_      = 0x74;
-        const uint8_t cmd_NULL_echo_        = 0x63;
-        const uint8_t cmd_NULL_no_echo_     = 0x77;
-        
+        const uint8_t example_broadcast_       = 0x00;
+        const uint8_t exmaple_number_          = 0x01;
+        const uint8_t example_motor_id_        = 0x01;
+        const uint8_t example_function_code_   = 0x65;
+        const uint8_t example_motor_drive_cmd_ = 0x63;
+
+
         const int rcv_size                  =    8;
 
 
     public:
-        MotorDriver(const std::string _serial_port, const int _baud_rate);
-        virtual ~MotorDriver();
+        /**
+         * @brief Constructor.
+         * @param serial_port port name, e.g. "/dev/ttyUSB0".
+         * @param baud_rate baud rate, e.g. 115200.
+         */
+        MotorDriver(const std::string serial_port, const int baud_rate);
 
-        // 這邊要更新成 id 也作為參數的版本
-        void ISTOP(uint8_t motor_id, bool is_echo);
-        void JG(uint8_t motor_id, uint16_t _cmd_rpm, bool is_echo);
-        void FREE(uint8_t motor_id, bool is_echo);
-        void SVON(uint8_t motor_id, bool is_echo);
-        void SVOFF(uint8_t motor_id, bool is_echo);
-        void IMR(uint8_t motor_id, uint16_t index, uint16_t step, bool is_echo);
-        void CS(uint8_t motor_id, uint16_t index, uint16_t step, bool is_echo);
-        void CMR(uint8_t motor_id, uint16_t index, uint16_t step, bool is_echo);
-        void CMA(uint8_t motor_id, uint16_t index, uint16_t step, bool is_echo);
-        void NULL_TO_ECHO(uint8_t motor_id, bool is_echo);
+        /**
+         * @brief Destructor.
+         */
+        ~MotorDriver();
 
+        /**
+         * @brief Example of using serial port communication & modbus protocol to write command to motor driver.
+         * @param data Communication data.
+         */
 
-    private:
-        
-        const uint8_t fc_master_send_cmd_Lite_  = 0x41;
-        const uint8_t cmd_ISTOP_Lite_           = 0x00;
-        const uint8_t cmd_JG_Lite_              = 0x01;
-        const uint8_t cmd_FREE_Lite_            = 0x05;
-        const uint8_t cmd_SVON_Lite_            = 0x06;
-        const uint8_t cmd_SVOFF_Lite_           = 0x07;
-        const uint8_t cmd_ALM_RST_Lite_         = 0x08;
-        const uint8_t cmd_BRAKE_Lite_           = 0x09;
-        const uint8_t cmd_NULL_Lite_            = 0x63;
-        const uint8_t echo_bit_Lite_            = 0x7F; 
-        const uint8_t no_echo_bit_Lite_         = 0x00; 
-        /*
-        Echo data:
-        1. Motor State
-        2. Hall Encoder Count
-        3. Motor Speed (rpm)
-        4. Error Code
-        5. I/O State
-        6. Voltage (V)
-        7. Current (A)
-        */
+        void exampleModbusCommand(uint16_t data);
 
 
-    public:
-        // 這邊要更新成 id 也作為參數的版本
-        void ISTOP_Lite(uint8_t motor_id, bool is_echo);
-        void JG_Lite(uint8_t motor_id, uint16_t _cmd_rpm, bool is_echo);
-        void FREE_Lite(uint8_t motor_id, bool is_echo);
-        void SVON_Lite(uint8_t motor_id, bool is_echo);
-        void SVOFF_Lite(uint8_t motor_id, bool is_echo);
-        void ALM_RST_Lite(uint8_t motor_id, bool is_echo);
-        void BRAKE_Lite(uint8_t motor_id, bool is_echo);
-        void NULL_Lite(uint8_t motor_id, bool is_echo);
-
-    public:
-        double get_encoder();
-        double get_current();
-        double get_voltage();
-        std::vector<double> get_motors_data(std::vector<uint8_t> drive_motors_id_list, std::vector<uint8_t> steer_motors_id_list);
-        bool FindSteeringHome(uint8_t motor_id);
+        /**
+         * @brief Example of using serial port communication & modbus protocol to read data from motor driver.
+         * 
+         */
+        double exampleGetEncoder();
 
 
-
-    public:
-        // For driving Wheels.
-        void Multi_ISTOP_Lite(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_JG_Lite(uint8_t num, std::vector<uint8_t> id_list, std::vector<int16_t> data_list, bool is_echo);
-
-        void Multi_FREE_Lite(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_SVON_Lite(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_SVOFF_Lite(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_ALM_RST_Lite(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_BRAKE_Lite(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_NULL_Lite(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-
-    public:
-        // For steering Wheels.
-        void Multi_ISTOP(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_JG(uint8_t num, std::vector<uint8_t> id_list, std::vector<int16_t> data_list, bool is_echo);
-
-        void Multi_FREE(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_SVON(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_SVOFF(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_NULL(uint8_t num, std::vector<uint8_t> id_list, bool is_echo);
-
-        void Multi_IMR(uint8_t num, std::vector<uint8_t> id_list, std::vector<int16_t> index_list, std::vector<uint16_t> step_list, bool is_echo);
-
-        void Multi_CS(uint8_t num, std::vector<uint8_t> id_list, std::vector<int16_t> index_list, std::vector<uint16_t> step_list, bool is_echo);
-
-        void Multi_CMR(uint8_t num, std::vector<uint8_t> id_list, std::vector<int16_t> index_list, std::vector<uint16_t> step_list, bool is_echo);
-
-        void Multi_CMA(uint8_t num, std::vector<uint8_t> id_list, std::vector<int16_t> index_list, std::vector<uint16_t> step_list, bool is_echo);
-
-        
-
+        /**
+         * @brief Computation of crc check code.
+         * @param data_vector The data vector used to compute crc16 check code.
+         */
+        inline uint16_t computeCRC16(std::vector<uint8_t> data_vector)
+        { 
+            uint16_t crc = 0xFFFF;  
+            for(auto idx=0; idx<data_vector.size(); idx++)
+            {
+                crc ^= (uint16_t)data_vector[idx];
+                for(auto i=8; i!=0; i--)
+                {
+                    if((crc & 0x0001) != 0)
+                    {
+                        crc >>= 1;
+                        crc ^= 0xA001;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+            }
+            return crc;
+        }
     };
 } // namespace Motor
 
--- include/serial_port_communication.h
+++ include/serial_port_communication.h
@@ -95,45 +95,6 @@ namespace Communication
              */
             std::vector<char> asyncReadDataThroughSerialPort(size_t data_size);
 
-
-            // void write_to_single_register(uint8_t id, uint8_t function_code, uint16_t _addr, uint16_t _data);
-            // std::vector<char> read_and_write(uint8_t _ID, uint8_t _FC, uint16_t _ADDR, uint16_t _DATA, int expected_bytes);
-            
-
-        // protected:
-            
-
-        //     inline uint16_t calculate_CRC(std::vector<uint8_t> _data){ 
-        //         uint16_t crc = 0xFFFF;  
-        //         for(auto idx=0; idx<_data.size(); idx++){
-        //             crc ^= (uint16_t)_data[idx];
-        //             for(auto i=8; i!=0; i--){
-        //                 if((crc & 0x0001) != 0){
-        //                     crc >>= 1;
-        //                     crc ^= 0xA001;
-        //                 }
-        //                 else{
-        //                     crc >>= 1;
-        //                 }
-        //             }
-        //         }
-        //         return crc;
-        //     }
-        //     inline bool validate_CRC(std::vector<char> _data){
-        //         std::vector<uint8_t> rcv_data(_data.begin(), _data.end()-2);
-        //         uint16_t crc = calculate_CRC(rcv_data);
-        //         uint8_t calc_crc_hi = crc;
-        //         uint8_t calc_crc_lo = crc >> 8;
-        //         uint8_t data_crc_hi = 0xFF & _data.at(_data.size() - 1) ;
-        //         uint8_t data_crc_lo = 0xFF & _data.at(_data.size() - 2);
-        //         if (data_crc_hi != calc_crc_hi || data_crc_lo != calc_crc_lo)
-        //         {
-        //             fprintf(stderr, "Invalid CRC. Received: %02x%02x, Calculated: %02x%02x\n",data_crc_hi, data_crc_lo, calc_crc_hi, calc_crc_lo);
-        //             return 0;
-        //         }
-        //         return 1;
-        //     }
-
     };
 }
 
