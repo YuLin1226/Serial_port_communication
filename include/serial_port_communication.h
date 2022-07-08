0e5ce41a53a63253d081baf3d95d847869d5a165
Ryan Chen <b04502132@ntu.edu.tw>
2022-07-08 09:49:52 +0800

    code style modification.

--- include/serial_port_communication.h
+++ include/serial_port_communication.h
@@ -11,79 +11,128 @@
 #define __DEBUG__ false
 #endif
 
-#ifndef MODBUS_H
-#define MODBUS_H
-
+#ifndef _SERIAL_PORT_COMMUNICATION_H_
+#define _SERIAL_PORT_COMMUNICATION_H_
 
 using namespace boost::asio;
 
-namespace Motor
+namespace Communication
 {
-    const int FAIL_READ_MAX_COUNT = 5;
-    const int RESPONSE_DELAY_US = 1500; // Default 3300 300us(C3.5) + 3ms(Tb2)
-    const int64_t READ_TIME_OUT_MS = 100; // 100ms for timeout
-    const int16_t MAX_ENC_DELTA = 4096;
-    const int16_t HALF_MAX_ENC_DELTA = 2048;
-    const int16_t ENC_RESOLUTION = 4096;
-    class SerialModbus
+    // Some global param used for serial port communication
+    const int FAIL_READ_MAX_COUNT       = 5;
+    const int RESPONSE_DELAY_US         = 1500; // Default 3300 300us(C3.5) + 3ms(Tb2)
+    const int64_t READ_TIME_OUT_MS      = 100;  // 100ms for timeout
+    const int16_t MAX_ENC_DELTA         = 4096;
+    const int16_t HALF_MAX_ENC_DELTA    = 2048;
+    const int16_t ENC_RESOLUTION        = 4096;
+
+    class SerialPort
     {
         
+        private:
+            // serial port name
+            std::string serial_port_;
+            
+            // baud rate
+            int baud_rate_;
+
+            // service
+            std::shared_ptr<io_service> service_;
+
+            // port
+            std::shared_ptr<serial_port> port_;
+
+            // mutex
+            std::shared_ptr<boost::mutex> mutex_;
+            // std::shared_ptr<boost::mutex> func_mutex_;
+            // std::mutex std_mutex_;
+
+            // timer
+            std::shared_ptr<deadline_timer> timeout_;
+
+            // data available in async read
+            bool data_available_;
+
         public:
-            SerialModbus(const std::string _serial_port, const int _baud_rate);
-            virtual ~SerialModbus();
+            /**
+             * @brief Constructor.
+             * @param serial_port port name, e.g. "/dev/ttyUSB0".
+             * @param baud_rate baud rate, e.g. 115200.
+             */
+            SerialPort(const std::string serial_port, const int baud_rate);
 
-            void set_SerialPort_Param(const std::string _serial_port, const int _baud_rate);
-            int open();
-            int close();
+            /**
+             * @brief Virtaul destructor, classes inherit this class should customize their own destructors.
+             */
+            virtual ~SerialPort();
 
-            void write(std::vector<char> _data);
-            void write_to_single_register(uint8_t _id, uint8_t _function_code, uint16_t _addr, uint16_t _data);
-            std::vector<char> asyncRead(size_t min_rcv);
-            std::vector<char> read_and_write(uint8_t _ID, uint8_t _FC, uint16_t _ADDR, uint16_t _DATA, int expected_bytes);
+            /**
+             * @brief Setting parameters.
+             * @param serial_port port name, e.g. "/dev/ttyUSB0".
+             * @param baud_rate baud rate, e.g. 115200.
+             */
+            void setParam(const std::string serial_port, const int baud_rate);
+
+            /**
+             * @brief Function of opening port.
+             */
+            int openSerialPort();
+
+            /**
+             * @brief Function of closing port.
+             */
+            int closeSerialPort();
+
+            /**
+             * @brief Function of writing data to target through serial port.
+             * @param data_vector a char vector containing data.
+             */
+            void writeDataThroughSerialPort(std::vector<char> data_vector);
+
+            /**
+             * @brief Function of receiving data in asynchronous way.
+             * @param data_size size of received data.
+             */
+            std::vector<char> asyncReadDataThroughSerialPort(size_t data_size);
+
+
+            // void write_to_single_register(uint8_t id, uint8_t function_code, uint16_t _addr, uint16_t _data);
+            // std::vector<char> read_and_write(uint8_t _ID, uint8_t _FC, uint16_t _ADDR, uint16_t _DATA, int expected_bytes);
             
 
-        protected:
-            std::shared_ptr<io_service> p_service;
-            std::shared_ptr<serial_port> p_port;
-            std::shared_ptr<boost::mutex> p_mutex;
-            std::shared_ptr<boost::mutex> p_func_mutex;
-            std::string p_serial_port;
-            int p_baud_rate;
-            std::mutex p_std_mutex;
-            std::shared_ptr<deadline_timer> p_timeout;
+        // protected:
+            
 
-            bool p_available;
-
-            inline uint16_t calculate_CRC(std::vector<uint8_t> _data){ 
-                uint16_t crc = 0xFFFF;  
-                for(auto idx=0; idx<_data.size(); idx++){
-                    crc ^= (uint16_t)_data[idx];
-                    for(auto i=8; i!=0; i--){
-                        if((crc & 0x0001) != 0){
-                            crc >>= 1;
-                            crc ^= 0xA001;
-                        }
-                        else{
-                            crc >>= 1;
-                        }
-                    }
-                }
-                return crc;
-            }
-            inline bool validate_CRC(std::vector<char> _data){
-                std::vector<uint8_t> rcv_data(_data.begin(), _data.end()-2);
-                uint16_t crc = calculate_CRC(rcv_data);
-                uint8_t calc_crc_hi = crc;
-                uint8_t calc_crc_lo = crc >> 8;
-                uint8_t data_crc_hi = 0xFF & _data.at(_data.size() - 1) ;
-                uint8_t data_crc_lo = 0xFF & _data.at(_data.size() - 2);
-                if (data_crc_hi != calc_crc_hi || data_crc_lo != calc_crc_lo)
-                {
-                    fprintf(stderr, "Invalid CRC. Received: %02x%02x, Calculated: %02x%02x\n",data_crc_hi, data_crc_lo, calc_crc_hi, calc_crc_lo);
-                    return 0;
-                }
-                return 1;
-            }
+        //     inline uint16_t calculate_CRC(std::vector<uint8_t> _data){ 
+        //         uint16_t crc = 0xFFFF;  
+        //         for(auto idx=0; idx<_data.size(); idx++){
+        //             crc ^= (uint16_t)_data[idx];
+        //             for(auto i=8; i!=0; i--){
+        //                 if((crc & 0x0001) != 0){
+        //                     crc >>= 1;
+        //                     crc ^= 0xA001;
+        //                 }
+        //                 else{
+        //                     crc >>= 1;
+        //                 }
+        //             }
+        //         }
+        //         return crc;
+        //     }
+        //     inline bool validate_CRC(std::vector<char> _data){
+        //         std::vector<uint8_t> rcv_data(_data.begin(), _data.end()-2);
+        //         uint16_t crc = calculate_CRC(rcv_data);
+        //         uint8_t calc_crc_hi = crc;
+        //         uint8_t calc_crc_lo = crc >> 8;
+        //         uint8_t data_crc_hi = 0xFF & _data.at(_data.size() - 1) ;
+        //         uint8_t data_crc_lo = 0xFF & _data.at(_data.size() - 2);
+        //         if (data_crc_hi != calc_crc_hi || data_crc_lo != calc_crc_lo)
+        //         {
+        //             fprintf(stderr, "Invalid CRC. Received: %02x%02x, Calculated: %02x%02x\n",data_crc_hi, data_crc_lo, calc_crc_hi, calc_crc_lo);
+        //             return 0;
+        //         }
+        //         return 1;
+        //     }
 
     };
 }
--- src/serial_port_communication.cpp
+++ src/serial_port_communication.cpp
@@ -1,107 +1,92 @@
-#include "include/modbus.h"
+#include "../include/serial_port_communication.h"
 
-#define WRITE_DELAY 10000
-
-namespace Motor
+namespace Communication
 {
-    SerialModbus::SerialModbus( const std::string _serial_port = "/dev/ttyUSB0",
-                                const int _baud_rate = 115200){
-        set_SerialPort_Param(_serial_port, _baud_rate);
+    SerialPort::SerialPort(const std::string serial_port = "/dev/ttyUSB0", const int baud_rate = 115200)
+    {
+        setParam(serial_port, baud_rate);
     }
-    
-    SerialModbus::~SerialModbus(){
+
+    SerialPort::~SerialPort()
+    {
         close();
     }
-
-    void SerialModbus::set_SerialPort_Param(const std::string _serial_port, const int _baud_rate){
-        p_serial_port   = _serial_port;
-        p_baud_rate     = _baud_rate;
+    
+    void SerialPort::setParam(const std::string serial_port, const int baud_rate)
+    {
+        serial_port_   = serial_port_;
+        baud_rate_     = baud_rate_;
     }
 
-    int SerialModbus::open(){
-        if(p_port != NULL)
+    int SerialPort::openSerialPort()
+    {
+        if(port_ != NULL)
         {
             return -1;
         }
-        if(p_service)
+        if(service_)
         {
-            p_port.reset();
-            p_service.reset();
+            port_.reset();
+            service_.reset();
         }
-        p_mutex     = std::shared_ptr<boost::mutex>{new boost::mutex};
-        p_service   = std::shared_ptr<io_service>{new io_service()};
-        p_port      = std::shared_ptr<serial_port>{ new serial_port(*p_service) };
-        p_timeout   = std::shared_ptr<deadline_timer>{new deadline_timer(*p_service)};
+
+        mutex_     = std::shared_ptr<boost::mutex>{new boost::mutex};
+        service_   = std::shared_ptr<io_service>{new io_service()};
+        port_      = std::shared_ptr<serial_port>{ new serial_port(*service_) };
+        timeout_   = std::shared_ptr<deadline_timer>{new deadline_timer(*service_)};
 
         try 
         {
-            /* ======================= 設置Serial Port參數 ======================= */
-            p_port->open(p_serial_port);
-            p_port->set_option(serial_port_base::baud_rate(p_baud_rate));
-            p_port->set_option(serial_port_base::character_size(8));
-            p_port->set_option(serial_port_base::parity(serial_port_base::parity::none));
-            p_port->set_option(serial_port_base::stop_bits(serial_port_base::stop_bits::one));
-            std::cout << "Serial Connect." << std::endl;
+            // Set port parameters.
+            port_->open(serial_port_);
+            port_->set_option(serial_port_base::baud_rate(baud_rate_));
+            port_->set_option(serial_port_base::character_size(8));
+            port_->set_option(serial_port_base::parity(serial_port_base::parity::none));
+            port_->set_option(serial_port_base::stop_bits(serial_port_base::stop_bits::one));
+            std::cout << ">>> Serial Port Connect." << std::endl;
         }
-        catch (std::exception &ex){
-            std::cout << "Open Exception : " << ex.what() << std::endl;
+        catch (std::exception &ex)
+        {
+            std::cout << ">>> Open Exception : " << ex.what() << std::endl;
         }
-        return 0;           
+        return 0;       
     }
 
-    int SerialModbus::close(){
-        if(p_port && p_port->is_open())
+    int SerialPort::closeSerialPort()
+    {
+        if(port_ && port_->is_open())
         {
-            p_port->close();
+            port_->close();
         }
-        std::cout << "Serial Disconnect." << std::endl;
-        return 0;           
+        std::cout << ">>> Serial Port Disconnect." << std::endl;
+        return 0;      
     }
 
-    void SerialModbus::write(std::vector<char> _data){
-	    if (p_port->is_open())
+
+    void SerialPort::writeDataThroughSerialPort(std::vector<char> data_vector)
+    {
+	    if (port_->is_open())
         {
-            boost::mutex::scoped_lock lock(*p_mutex);
-            auto size = p_port->write_some(buffer(_data));
-            if(_data.size() != size)
+            boost::mutex::scoped_lock lock(*mutex_);
+            auto size = port_->write_some(buffer(data_vector));
+            if(data_vector.size() != size)
             {
-                throw "Write Size Error.";
+                throw ">>> Write Size Error.";
             }      
         }
         else
         {
-            throw "Serial Port Is Not Open.";
+            throw ">>> Serial Port Is Not Open.";
         }
     }    
     
-    void SerialModbus::write_to_single_register(uint8_t _id, uint8_t _function_code, uint16_t _addr, uint16_t _data)
-    {
-        /* =====================================================================================
-            * 建立Bytes資料容器
-            * 存入 ID, FunctionCode, Address, Data, CRC
-            * 轉換Char資料容器
-            * 發送資料
-           ===================================================================================== */
-        std::vector<uint8_t> p_data;
-        p_data.clear();
-        p_data.push_back(_id);
-        p_data.push_back(_function_code);
-        p_data.push_back(_addr >> 8);
-        p_data.push_back(_addr);
-        p_data.push_back(_data >> 8);
-        p_data.push_back(_data);
-        uint16_t crc = this->calculate_CRC(p_data);
-        p_data.push_back(crc >> 8);
-        p_data.push_back(crc);
-        std::vector<char> p_char(p_data.begin(), p_data.end());
-        this->write(p_char);
-    }
+    
 
-    std::vector<char> SerialModbus::asyncRead(size_t rcv_size)
+    std::vector<char> SerialPort::asyncReadDataThroughSerialPort(size_t data_size)
     {
-        p_service->reset();
-        p_available = false;
-        std::vector<char> p_char(rcv_size);
+        service_->reset();
+        data_available_ = false;
+        std::vector<char> char_vector(data_size);
         try
         {
             /* =============================== 邏輯解釋 ===============================
@@ -118,61 +103,86 @@ namespace Motor
                 1-2) 讀取 -> 失敗 -> 拋出錯誤
                 2-1) 定時器 -> 過期 -> 取消序列埠工作 -> 拋出錯誤              
                =============================== 邏輯解釋 ===============================*/
-            boost::mutex::scoped_lock sl(*p_mutex);
-            async_read( *p_port, 
-                        boost::asio::buffer(p_char, rcv_size),
+            boost::mutex::scoped_lock scoped_locker(*mutex_);
+            async_read( *port_, 
+                        boost::asio::buffer(char_vector, data_size),
                         [&](const boost::system::error_code &error, std::size_t bytes_transferred)
                         {
                             if (error)
                             {
-                                p_available = false;
-                                std::cerr << "readCallback Error " << error << std::endl;
+                                data_available_ = false;
+                                std::cerr << ">>> readCallback Error " << error << std::endl;
                             }
-                            p_timeout->cancel();
-                            p_available = true;
+                            timeout_->cancel();
+                            data_available_ = true;
                         });
-            p_timeout->expires_from_now(boost::posix_time::millisec(READ_TIME_OUT_MS));
-            p_timeout->async_wait(  [&](const boost::system::error_code &error)
+            timeout_->expires_from_now(boost::posix_time::millisec(READ_TIME_OUT_MS));
+            timeout_->async_wait(  [&](const boost::system::error_code &error)
                                     {
                                         if (!error)
                                         {
-                                            p_available = false;
-                                            p_port->cancel(); 
-                                            std::cerr << "Read timeout" << std::endl;
+                                            data_available_ = false;
+                                            port_->cancel(); 
+                                            std::cerr << ">>> Read timeout." << std::endl;
                                         }
                                     });
-            p_service->run(); 
+            service_->run(); 
         }
         catch(const std::exception& ex)
         {
-            std::cout << "Read exception. " << ex.what() << std::endl;
+            std::cout << ">>> Read exception. " << ex.what() << std::endl;
         }
-        if (p_available)
+        if (data_available_)
         {
-            return p_char;
+            return char_vector;
         }
         else
         {
-            throw "Serial port reading timeout";
+            throw ">>> Serial port reading timeout";
         }
     }
-
-    std::vector<char> SerialModbus::read_and_write(uint8_t _ID, uint8_t _FC, uint16_t _ADDR, uint16_t _DATA, int expected_bytes){
     
-        /* 這個功能之後會作為特定函數的模板，例如：get_MotorSpeed，那就是先寫入再讀取 */
-        write_to_single_register(_ID, _FC, _ADDR, _DATA);    
-        std::vector<char> response;
-        {
-            usleep(RESPONSE_DELAY_US);
-            try
-            {
-                response = asyncRead(expected_bytes);
-            }
-            catch(const std::exception& e)
-            {
-                std::cerr << e.what() << '\n';
-            }
-        }
-        return response;
-    }
+    // void SerialModbus::write_to_single_register(uint8_t _id, uint8_t _function_code, uint16_t _addr, uint16_t _data)
+    // {
+    //     /* =====================================================================================
+    //         * 建立Bytes資料容器
+    //         * 存入 ID, FunctionCode, Address, Data, CRC
+    //         * 轉換Char資料容器
+    //         * 發送資料
+    //        ===================================================================================== */
+    //     std::vector<uint8_t> p_data;
+    //     p_data.clear();
+    //     p_data.push_back(_id);
+    //     p_data.push_back(_function_code);
+    //     p_data.push_back(_addr >> 8);
+    //     p_data.push_back(_addr);
+    //     p_data.push_back(_data >> 8);
+    //     p_data.push_back(_data);
+    //     uint16_t crc = this->calculate_CRC(p_data);
+    //     p_data.push_back(crc >> 8);
+    //     p_data.push_back(crc);
+    //     std::vector<char> p_char(p_data.begin(), p_data.end());
+    //     this->write(p_char);
+    // }
+
+    
+
+    // std::vector<char> SerialModbus::read_and_write(uint8_t _ID, uint8_t _FC, uint16_t _ADDR, uint16_t _DATA, int expected_bytes){
+    
+    //     /* 這個功能之後會作為特定函數的模板，例如：get_MotorSpeed，那就是先寫入再讀取 */
+    //     write_to_single_register(_ID, _FC, _ADDR, _DATA);    
+    //     std::vector<char> response;
+    //     {
+    //         usleep(RESPONSE_DELAY_US);
+    //         try
+    //         {
+    //             response = asyncRead(expected_bytes);
+    //         }
+    //         catch(const std::exception& e)
+    //         {
+    //             std::cerr << e.what() << '\n';
+    //         }
+    //     }
+    //     return response;
+    // }
 }
