26b58e78bb75ca974530f5921c5e1ada913fb390
Ryan Chen <b04502132@ntu.edu.tw>
2022-07-08 10:40:15 +0800

    update test node

--- src/test/motor_test_node.cpp
+++ src/test/motor_test_node.cpp
@@ -1,65 +1,28 @@
-// #include "trumman/trumman.h"
-// #include <ros/ros.h>
-// #include <std_msgs/Float64.h>
-#include "include/modbus.h"
-#include "include/motor_driver.h"
+#include "../../include/motor_driver/motor_driver.h"
 #include <iostream>
 #include <string>
 #include <memory>
 #include <thread>
 #include <boost/thread/mutex.hpp>
 
-
-
 int main(int argc, char **argv)
 {
-    // std::shared_ptr<Motor::SerialModbus> p_motor = std::make_shared<Motor::SerialModbus>(argc > 1 ? argv[1] : "/dev/pts/8", 115200);
+    // Create object pointer.
+    std::shared_ptr<AMR::MotorDriver> test_node_motor = std::make_shared<AMR::MotorDriver>(argc > 1 ? argv[1] : "/dev/ttyUSB0", 115200);
 
-    std::shared_ptr<Motor::MotorDriver> p_motor = std::make_shared<Motor::MotorDriver>(argc > 1 ? argv[1] : "/dev/ttyUSB0", 115200);
+    // Open serial port.
+    test_node_motor->openSerialPort();
 
-    // 開啟通訊
-    p_motor->open();
-
-    // if(true){
-    //     try
-    //     {
-    //         p_motor -> JG(300, false);
-    //         // p_motor -> NULL_TO_ECHO(true);
-    //         sleep(5);
-    //         // p_motor -> SetMotorSpeed_RPM(0, false);
-    //     }
-    //     catch(const std::exception& e)
-    //     {
-    //         std::cerr << e.what() << '\n';
-    //     }
-        
-    // }
+    // Write cmd.
+    uint16_t example_data = 0x1234;
+    test_node_motor->exampleModbusCommand(example_data);
+    sleep(1);
     
-    p_motor -> CS(0x01, 0, 0, false); // init encoder count
-    sleep(1);
+    // Read data
+    auto example_encoder_value = test_node_motor->exampleGetEncoder();
 
-    p_motor -> JG(0x01, 300, false);
-    // p_motor -> CMR(10, 5000, false);
-    sleep(1);
-    for(auto i=0; i<10; i++){
-        try
-        {
-            // p_motor -> NULL_TO_ECHO(true);
-            double enc_data_ = p_motor->get_encoder();
-            std::cout << std::dec << "No. " << i+1 << " loop, data: " << enc_data_ << "\n" << std::endl;
-            sleep(1);
-            // // p_motor -> SetMotorSpeed_RPM(0, false);
-            
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-        
-    }
-
-    // 關閉通訊
-    // p_motor->close();
+    // Manually close serial port. Or you can alternatively let the destructor to do so.
+    test_node_motor->closeSerialPort();
     return 0;
 }
 
